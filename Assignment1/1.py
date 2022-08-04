259172ef16e6f11a6aa12eaad38a58ad6921093d
Rituparna W <rituparnaw16@gmail.com>
2022-08-04 21:42:34 +0530

    2

---
+++ Assignment1/1.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def splitString(string:str)->List[str]:
+  """Function to break down a string into a list of characters..
+  doctests:
+  >>> splitString("Ritu")
+  ['R', 'i', 't', 'u']
+  >>> splitString("Rituparna")
+  ['R', 'i', 't', 'u', 'p', 'a', 'r', 'n', 'a']
+  """
+
+  return list(string)
+
+
+if __name__=="__main__":
+  import doctest
+  doctest.testmod()
+
+  print(splitString("rwarwatkar"))

f0af6935625d977465c74eca7962d81f3c04cae8
Rituparna W <rituparnaw16@gmail.com>
2022-08-05 15:54:13 +0530

    7

--- Assignment1/7.py
+++ Assignment1/7.py
@@ -2,9 +2,23 @@ from typing import List
 
 
 def getFirstRepeatingElement(inputList: List[int]) -> int:
+    """Function to get the first repeating element from a list
+    doctests:
+    >>> inputList = [1, 2, 1, 1, 2, 2, 3, 4, 5, 1, 1, 2, 2]
+    >>> first = getFirstRepeatingElement(inputList)
+    >>> print(first)
+    1
+    """
     hash = set()
     for i in inputList:
         if hash.__contains__(i) == True:
             return i
         else:
             hash.add(i)
+    return float("-inf")
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()

34d992d673fbf3a5daf9fd139efb8edbbf82c3dc
Rituparna W <rituparnaw16@gmail.com>
2022-08-23 21:39:13 +0530

    Assignment 3

---
+++ Assignment3/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y httpd.x86_64 awscli
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+mkdir ~/.aws
+echo "[default]\nregion = ap-south-1" > ~/.aws/config
+echo "[default]\naws_access_key_id = key\naws_secret_access_key = secret" > ~/.aws/credentials
+aws s3 cp s3://xd-dead/ /var/www/html --recursive
